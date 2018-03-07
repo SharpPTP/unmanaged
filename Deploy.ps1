f01e669be87aa37d48d3b1a42753d1aa6f953573
Petar Petrov <ppetrov@david.bg>
2018-03-07 16:42:01 +0200

    Minor build improvements

--- Deploy.ps1
+++ Deploy.ps1
@@ -4,4 +4,6 @@ $packagesPath = Join-Path -Path $scriptPath -ChildPath ".deploy\packages"
 
 Write-Host "Clearing deploy folder" | Remove-Item -Recurse -Force $deployPath
 
+Write-Host "Testing" | dotnet test test/Unmanaged.Test/Unmanaged.Test.csproj -c Release
+
 Write-Host "Deploying nuget packages" | dotnet pack -o $packagesPath
